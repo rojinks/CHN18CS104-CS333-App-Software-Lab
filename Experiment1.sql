346223b0442c0c4e4daa3fecc089e87d0c80a837
Rojin K Sunny <49112755+rojinks@users.noreply.github.com>
2020-12-05 16:49:12 +0530

    Update Experiment1.sql

--- Experiment1.sql
+++ Experiment1.sql
@@ -1,24 +1,26 @@
 /* CHN18CS104 */
 /* Rojin K Sunny */
 /* CS C 51 */
-/* ---------------------------------------------------------- */
+/* ------------------------------------------------------------- */
 /* Experiment 1: mySQL Commands */
 /* ------------------------------------------------------------- */
-/*a)*/    CREATE TABLE Student(
-                Sno INT NOT NULL PRIMARY KEY,
-                Name VARCHAR(50) NOT  NULL,
-                Marks INT NOT NULL,
-                Dept VARCHAR(25) NOT NULL);
+
+CREATE TABLE Student(
+    Sno INT NOT NULL PRIMARY KEY,
+    Name VARCHAR(50) NOT  NULL,
+    Marks INT NOT NULL,
+    Dept VARCHAR(25) NOT NULL);
     
-/*b)*/    ALTER TABLE Student ADD age INT;
+ALTER TABLE Student ADD age INT;
 
-/*c)*/    ALTER TABLE Student MODIFY COLUMN Dept VARCHAR(55);
+ALTER TABLE Student MODIFY COLUMN Dept VARCHAR(55);
 
-/*d)*/    ALTER TABLE Student DROP COLUMN Marks;
+ALTER TABLE Student DROP COLUMN Marks;
 
-/*e)*/    ALTER TABLE Student RENAME TO Students;
+ALTER TABLE Student RENAME TO Students;
 
-/*f)*/    TRUNCATE TABLE Students;
+TRUNCATE TABLE Students;
 
-/*g)*/    DROP TABLE Students;
-/* ---------------------------------------------------------- */
+DROP TABLE Students;
+
+/* ------------------------------------------------------------- */
