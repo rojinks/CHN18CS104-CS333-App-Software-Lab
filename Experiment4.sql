85f650012ca348d97226da6aaae2a4afbc6a469d
Rojin K Sunny <49112755+rojinks@users.noreply.github.com>
2020-12-05 16:53:34 +0530

    Update and rename Experiment4 to Experiment4.sql

--- Experiment4.sql
+++ Experiment4.sql
@@ -1,26 +1,26 @@
-CHN18CS104
-Rojin K Sunny
-CS C 51
----------------------------------------------------------------
-Experiment 4: mySQL Commands
----------------------------------------------------------------
+/* CHN18CS104 */
+/* Rojin K Sunny */
+/* CS C 51 */
+/* --------------------------------------------------------------- */
+/* Experiment 4: mySQL Commands
+/* --------------------------------------------------------------- */
 
-a)    CREATE TABLE Department(
+CREATE TABLE Department(
           Code INT PRIMARY KEY NOT NULL,
           Title VARCHAR(30),
           Dept_name VARCHAR(30) UNIQUE NOT NULL,
           Dept_id INT UNIQUE NOT NULL,
           Salary INT, CHECK (Salary > 2000) );
 
-b)    CREATE TABLE Instructor(
+CREATE TABLE Instructor(
           Name VARCHAR(30) NOT NULL,
           Code INT NOT NULL,
           Id INT PRIMARY KEY DEFAULT 0 );
 
-c)    INSERT INTO Department
+INSERT INTO Department
           VALUES (69, "Grandmaster", "Hashashin", 666, 3000);
 
-d)    INSERT INTO Instructor
+INSERT INTO Instructor
           VALUES ("Altair Ibn-La'Ahad", 14, 14);
 
----------------------------------------------------------------
+/* --------------------------------------------------------------- */
