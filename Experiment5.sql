2a3643a995d62f1f473760ef1e03085867fe796c
Rojin K Sunny <49112755+rojinks@users.noreply.github.com>
2020-12-05 16:59:31 +0530

    Create Experiment5.sql

---
+++ Experiment5.sql
@@ -0,0 +1,41 @@
+/* CHN18CS104 */
+/* Rojin K Sunny */
+/* CS C 51 */
+/* ------------------------------------------------------------------------ */
+/* Experiment 5: mySQL TCL Commands
+/* ------------------------------------------------------------------------ */
+
+USE applab;
+
+CREATE TABLE Class(
+        id INT PRIMARY KEY,
+        std_name VARCHAR(50) );
+    
+START TRANSACTION;
+
+SET autocommit=0;
+
+INSERT INTO  Class
+        VALUES (01, "Altair"), (02, "Ezio"), (03, "Connor"), (04, "Edward");
+
+COMMIT;
+
+INSERT INTO Class
+        VALUES (05, "Shay");
+ 
+SELECT * FROM Class;
+
+ROLLBACK;
+
+SAVEPOINT A;
+
+INSERT INTO Class
+        VALUES (06, "Desmond");
+
+SELECT * FROM Class;
+
+ROLLBACK TO A;
+
+SELECT * FROM Class;
+
+/* ------------------------------------------------------------------------ */
